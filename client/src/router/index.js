cb183dacb16a2945849db68e16078541781ae648
空^白 <binit@sina.cn>
2018-04-16 22:15:47 +0700

    添加路由注释

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -7,6 +7,7 @@ import Login from  '@/components/Login'
 import UserCenter from  '@/components/user/Index'
 import MyPublish from  '@/components/user/MyPublish'
 import Profile from  '@/components/user/Profile'
+import ModifyAvatar from  '@/components/user/ModifyAvatar'
 
 Vue.use(Router)
 
@@ -18,23 +19,23 @@ export default new Router({
       component: Index,
       redirect: 'list',
       children: [
-        {
+        { //默认首页
           path: "/list",
           name: 'index',
           component: List
         },
-        {
+        { // 文章详情
           path: '/list/:user/:article_flag',
           name: 'article',
           component: Show
         },
       ]
     },
-    {
+    { // 用户中心
       path: '/user',
       component: UserCenter,
       children:[
-        {
+        { // 默认用户中心页面
           path: '/user/:user_id',
           name: 'user',
           component: MyPublish,
@@ -42,7 +43,7 @@ export default new Router({
             requiredAuth: true
           }
         },
-        {
+        { // 用户修改个人资料
           path: 'profile/:user_id',
           name: 'user_profile',
           component: Profile,
@@ -50,7 +51,15 @@ export default new Router({
             requiredAuth: true
           }
         },
-        {
+        { // 用户修改头像
+          path: 'edit_avatar/:user_id',
+          name: 'user_profile',
+          component: ModifyAvatar,
+          meta: {
+            requiredAuth: true
+          }
+        },
+        { // 我的文章
           path: 'my_article/user_id',
           name: 'user_article',
           meta: {
@@ -59,7 +68,7 @@ export default new Router({
         }
       ]
     },
-    {
+    { // 登录
       path: '/login',
       name: 'login',
       component: Login
