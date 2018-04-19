cf6f6b7662a276337b530cc5b120a6c5f6079fce
siskinc <susecjh@gmail.com>
2018-04-19 08:06:23 +0800

    修改教程页面bug

--- client/src/components/Courses.vue
+++ client/src/components/Courses.vue
@@ -41,28 +41,30 @@
       return {
         items: [
           {
-            image: "https://fsdhub.com/uploads/images/201704/28/1/O2sXyZ5ZH6.jpg?imageView2/1/w/200/h/200",
+            image: "https://www.baidu.com",
             header: "Laravel 教程 - Web 开发实战入门 ( Laravel 5.5 )",
-            description: "本书将教你如何使用 Laravel 一步一步构建一个类似新浪微博的应用，让你从实际开发中体会到 Laravel 开发的敏捷、愉悦与轻松。<br>\n" +
-            "              通过阅读本教程，你将学到如 HTML、CSS、JavaScript、PHP 和 Laravel 等 Web 开发相关的基础知识。本书还会对这些基础知识点进行延伸扩展，为你讲解一些在 Web\n" +
-            "              开发中更为专业、实用的技能，如 Git 工作流、Gulp 前端工作流、Bootstrap\n" +
-            "              框架基本使用等。这些知识将为你未来的编程开发奠定下坚实的基础。使你不论是在做自己的个人项目，或是构建一个伟大的商业产品时，都能得心应手。"
+            description: "本书将教你如何使用 Laravel 一步一步构建一个类似新浪微博的应用，让你从实际开发中体会到 Laravel 开发的敏捷、愉悦与轻松。<br>" +
+            "通过阅读本教程，你将学到如 HTML、CSS、JavaScript、PHP 和 Laravel 等 Web 开发相关的基础知识。本书还会对这些基础知识点进行延伸扩展，为你讲解一些在 Web" +
+            "开发中更为专业、实用的技能，如 Git 工作流、Gulp 前端工作流、Bootstrap" +
+            "框架基本使用等。这些知识将为你未来的编程开发奠定下坚实的基础。使你不论是在做自己的个人项目，或是构建一个伟大的商业产品时，都能得心应手。"
           },
           {
             image: "https://fsdhub.com/uploads/images/201704/28/1/O2sXyZ5ZH6.jpg?imageView2/1/w/200/h/200",
-            header: "Laravel 教程 - Web 开发实战入门 ( Laravel 5.5 )",
-            description: "本书将教你如何使用 Laravel 一步一步构建一个类似新浪微博的应用，让你从实际开发中体会到 Laravel 开发的敏捷、愉悦与轻松。<br>\n" +
-            "              通过阅读本教程，你将学到如 HTML、CSS、JavaScript、PHP 和 Laravel 等 Web 开发相关的基础知识。本书还会对这些基础知识点进行延伸扩展，为你讲解一些在 Web\n" +
-            "              开发中更为专业、实用的技能，如 Git 工作流、Gulp 前端工作流、Bootstrap\n" +
-            "              框架基本使用等。这些知识将为你未来的编程开发奠定下坚实的基础。使你不论是在做自己的个人项目，或是构建一个伟大的商业产品时，都能得心应手。"
+            header: "Laravel 教程 - Web 开发实战入门 ( Laravel 5 )",
+            description: "本书将教你如何使用 Laravel 一步一步构建一个类似新浪微博的应用，让你从实际开发中体会到 Laravel 开发的敏捷、愉悦与轻松。<br>" +
+            "通过阅读本教程，你将学到如 HTML、CSS、JavaScript、PHP 和 Laravel 等 Web 开发相关的基础知识。本书还会对这些基础知识点进行延伸扩展，为你讲解一些在 Web" +
+            "开发中更为专业、实用的技能，如 Git 工作流、Gulp 前端工作流、Bootstrap" +
+            "框架基本使用等。这些知识将为你未来的编程开发奠定下坚实的基础。使你不论是在做自己的个人项目，或是构建一个伟大的商业产品时，都能得心应手。"
           },
           {
             image: "https://fsdhub.com/uploads/images/201704/28/1/O2sXyZ5ZH6.jpg?imageView2/1/w/200/h/200",
-            header: "Laravel 教程 - Web 开发实战入门 ( Laravel 5.5 )",
-            description: "本书将教你如何使用 Laravel 一步一步构建一个类似新浪微博的应用，让你从实际开发中体会到 Laravel 开发的敏捷、愉悦与轻松。<br>\n" +
-            "              通过阅读本教程，你将学到如 HTML、CSS、JavaScript、PHP 和 Laravel 等 Web 开发相关的基础知识。本书还会对这些基础知识点进行延伸扩展，为你讲解一些在 Web\n" +
-            "              开发中更为专业、实用的技能，如 Git 工作流、Gulp 前端工作流、Bootstrap\n" +
-            "              框架基本使用等。这些知识将为你未来的编程开发奠定下坚实的基础。使你不论是在做自己的个人项目，或是构建一个伟大的商业产品时，都能得心应手。"
+            header: "Laravel 教程 - Web 开发实战入门 ( Laravel 5.7 )",
+            description: "本书将教你如何使用 Laravel 一步一步构建一个类似新浪微博的应用，让你从实际开发中体会到 Laravel 开发的敏捷、愉悦与轻松。<br>" +
+            "通过阅读本教程，你将学到如 HTML、CSS、JavaScript、PHP 和 Laravel 等 Web 开发相关的基础知识。本书还会对这些基础知识点进行延伸扩展，为你讲解一些在 Web" +
+            "开发中更为专业、实用的技能，如 Git 工作流、Gulp 前端工作流、Bootstrap" +
+            "框架基本使用等。这些知识将为你未来的编程开发奠定下坚实的基础。使你不论是在做自己的个人项目，或是构建一个伟大的商业产品时，都能得心应手。" +
+            "asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd" +
+            "asdddddddddddddddddddddasdsadasdasdssaddddddddddddddddddddddddddddddddddddddddddddddddddd"
           }
         ]
       }
@@ -82,8 +84,9 @@
     // alert("size:" + size);
     for(var i = 0; i < size; i = i+1) {
       var contentHeight = contents[i].clientHeight;
+      console.log(contentHeight);
       // console.log(items_[i]);
-      items_[i].style.height =  contentHeight + 50 + "px";
+      items_[i].style.height =  contentHeight * (1 + 0.3) + "px";
     }
   }
 </script>
@@ -109,7 +112,7 @@
   }
 
   .courses-item {
-    width: 60%;
+    width: 80%;
     height: 300px;
     padding: 2%;
     margin: 0 auto;
@@ -126,7 +129,7 @@
 
   .content {
     margin-left: 13%;
-    width: 45%;
+    width: 80%;
     position: absolute;
   }
 
@@ -138,5 +141,7 @@
   .description {
     padding: 2%;
     font-weight: bold;
+    width: 80%;
+    word-wrap: break-word;
   }
 </style>
